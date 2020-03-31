f1627a39b109e6cced876ea3459e68f2ced1a364
GHooN99 <whiteblue0316@gmail.com>
2020-03-31 22:08:48 +0900

    Add visual_micro_test.ino

---
+++ 아두이노기초/visual_micro_test.ino
@@ -0,0 +1,19 @@
+﻿// vs2017 , visual micro 작동 테스트 
+void setup() 
+{
+	Serial.begin(9600);
+
+}
+// the loop function runs over and over again until power down or reset
+void loop() 
+{
+	Serial.println("hello");
+	delay(1000);
+	printNum(10);
+
+}
+void printNum(int x)
+{
+	Serial.println(x);
+	delay(100);
+}
